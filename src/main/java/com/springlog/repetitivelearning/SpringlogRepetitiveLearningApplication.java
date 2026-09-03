af02c1401975b111e57dbeb893b5d0c0bcea2a96
jangjunseo518-collab <wkdwnstj00@naver.com>
2026-09-03 14:19:49 +0900

    반복 학습 프로젝트 초기설정 완료

---
+++ src/main/java/com/springlog/repetitivelearning/SpringlogRepetitiveLearningApplication.java
@@ -0,0 +1,13 @@
+package com.springlog.repetitivelearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlogRepetitiveLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringlogRepetitiveLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springlog/repetitivelearning/SpringlogRepetitiveLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springlog.repetitivelearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringlogRepetitiveLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
